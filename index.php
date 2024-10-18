0db31475d8e08debf21be5100701a3066cc9a19d
Ferney Barbosa <barbosaferney05@gmail.com>
2024-10-18 14:40:13 -0500

    update - vendex 0.5

--- index.php
+++ index.php
@@ -18,8 +18,8 @@
                 </div>
 
                 <div class="buttons">
-                    <a href="#" class="access">Ingresar</a>
-                    <a href="#" class="access">Regístrate</a>
+                    <a href="#" id="show-login" class="access">Ingresar</a>
+                    <a href="#" id="show-register" class="access">Regístrate</a>
                 </div>
 
                 <div class="menu">
@@ -28,14 +28,16 @@
             </div>
         </nav>
 
+        <!-- MODAL BUTTONS -->
+
         <section id="modal-buttons">
             <div class="modal-buttons">
                 <div class="close">
                     <img src="svg/close-x.svg" id="close-x" alt="">
                 </div>
 
-                <a href="#" class="access-modal">Ingresar</a>
-                <a href="#" class="access-modal">Regístrate</a>
+                <a href="#" id="show-login-buttons" class="access-modal">Ingresar</a>
+                <a href="#" id="show-register-buttons" class="access-modal">Regístrate</a>
             </div>
         </section>
 
@@ -47,6 +49,58 @@
                 </div>
 
                 <button type="button">Iniciar ahora</button>
+            </div>
+        </section>
+
+        <!-- MODAL REGISTER -->
+
+        <section id="modal-register">
+            <div class="content-logo-form">
+                <div class="logo-form">
+                    <div class="logo-close">
+                        <div class="none-logo">
+                            <div class="logo-icon">
+                                <img src="svg/icon-vendex.svg" alt="Logo de Vendex">
+                                <h1>ENDEX</h1>
+                            </div>
+                        </div>
+
+                        <div class="close-modals">
+                            <svg xmlns="http://www.w3.org/2000/svg" id="close-register" width="30" height="35" viewBox="0 0 304 384"><path fill="#ffffff" d="M299 73L179 192l120 119l-30 30l-120-119L30 341L0 311l119-119L0 73l30-30l119 119L269 43z"/></svg>
+                        </div>
+                    </div>
+
+                    <form action="" method="POST" class="form">
+                        <div class="tlt-text-modal">
+                            <h1 class="tlt-form-modal">¡Bienvenido!</h1>
+                            <p class="text-form-modal">Completa el formulario para registrarte y comenzar a optimizar la gestión de tus inventarios.</p>
+                        </div>
+
+                        <div class="content-form">
+                            <div class="content-inputs">
+                                <input type="text" name="name" class="input" placeholder="Tu nombre" required>
+                                <input type="text" name="lastname" class="input" placeholder="Tu apellido" required>
+                            </div>
+
+                            <div class="content-inputs">
+                                <input type="email" name="email" class="input" placeholder="Tu email" required>
+                            </div>
+
+                            <div class="content-inputs">
+                                <input type="password" name="password" class="input" placeholder="Contraseña" required>
+                                <select name="rol" id="" class="select" required>
+                                    <option value="" disabled selected>Seleccione</option>
+                                    <option value="">Tienda</option>
+                                </select>
+                            </div>
+
+                            <div class="content-inputs">
+                                <input type="submit" name="button-register" class="button-form" value="Registrarte">
+                            </div>
+                        </div>
+
+                    </form>
+                </div>
             </div>
         </section>
     </main>
--- js/home.js
+++ js/home.js
@@ -1,19 +1,31 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const getElement = (id) => document.getElementById(id)
+    const getElement = (id) => document.getElementById(id);
 
     const toggleDisplay = (e) => {
-        e.style.display = (e.style.display === 'none') ? 'block' : 'none'
-    }
+        e.style.display = (e.style.display === 'none' || !e.style.display) ? 'block' : 'none';
+    };
 
-    const menu_modal = getElement('menu-modal')
-    const modal_buttons = getElement('modal-buttons')
-    const close_x = getElement('close-x')
+    const addToggleListener = (triggerElement, targetElement) => {
+        triggerElement.addEventListener('click', () => {
+            toggleDisplay(targetElement);
+        });
+    };
 
-    menu_modal.addEventListener('click', () => {
-        toggleDisplay(modal_buttons)
-    })
+    // MENU MODAL LOGIC
+    const menuModal = getElement('menu-modal');
+    const modalButtons = getElement('modal-buttons');
+    const closeX = getElement('close-x');
 
-    close_x.addEventListener('click', () => {
-        toggleDisplay(modal_buttons)
-    })
-}) 
+    addToggleListener(menuModal, modalButtons);
+    addToggleListener(closeX, modalButtons);
+
+    // MODAL REGISTER LOGIC
+    const showRegister = getElement('show-register');
+    const modalRegister = getElement('modal-register');
+    const closeRegister = getElement('close-register');
+    const showRegisterButtons = getElement('show-register-buttons');
+
+    addToggleListener(showRegister, modalRegister);
+    addToggleListener(showRegisterButtons, modalRegister);
+    addToggleListener(closeRegister, modalRegister);
+});
