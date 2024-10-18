0f2c6569bef9355ad8520b6127e824b3e3b26f35
Ferney Barbosa <barbosaferney05@gmail.com>
2024-10-18 11:20:34 -0500

    Uploading files - vendex 0.3

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Vendex - Gestión de Inventarios Inteligente</title>
+    <link rel="shortcut icon" href="SVG/icon-vendex.svg" type="image/x-icon">
+    <link rel="stylesheet" href="css/home.css">
+</head>
+<body>
+    
+    <main class="main-home">
+        <nav class="nav-home">
+            <div class="logo-buttons">
+                <div class="logo">
+                    <img src="svg/icon-vendex.svg" alt="Logo de Vendex">
+                    <h1>ENDEX</h1>
+                </div>
+
+                <div class="buttons">
+                    <a href="#" class="access">Ingresar</a>
+                    <a href="#" class="access">Regístrate</a>
+                </div>
+
+                <div class="menu">
+                    <img src="svg/menu.svg" alt="">
+                </div>
+            </div>
+        </nav>
+
+        <section id="modal-buttons">
+            <div class="modal-buttons">
+                <div class="close">
+                    <img src="svg/close-x.svg" id="close-x" alt="">
+                </div>
+                
+                <a href="#" class="access-modal">Ingresar</a>
+                <a href="#" class="access-modal">Regístrate</a>
+            </div>
+        </section>
+
+        <section class="welcome-home">
+            <div class="psb">
+                <div class="phrase-slogan">
+                    <h1 class="phrase">Gestiona tus inventarios con precisión y eficiencia, todo con Vendex.</h1>
+                    <p class="slogan">Tu socio estratégico en la gestión de inventarios, donde cada negocio encuentra el control y la eficiencia que necesita para crecer y prosperar.</p>
+                </div>
+
+                <button type="button">Iniciar ahora</button>
+            </div>
+        </section>
+    </main>
+
+</body>
+</html>
