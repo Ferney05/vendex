7b4425ca6a4e4838f0eb08127049f4de51d87a6b
Ferney Barbosa <barbosaferney05@gmail.com>
2024-10-19 21:22:27 -0500

    update - vendex 1.0

--- js/dashboard-store.js
+++ js/dashboard-store.js
@@ -1,18 +1,28 @@
-const getElementID = (id) => document.getElementById(id);
-const getElementClass = (className) => document.querySelectorAll(className);
+document.addEventListener('DOMContentLoaded', () => {
+    const getElementID = (id) => document.getElementById(id);
+    const getElementClass = (className) => document.querySelectorAll(className);
 
-const accordionBtns = getElementClass('.accordion-btn');
-const accordionContents = getElementClass('.accordion-content');
-const downArrows = getElementClass('.down-arrow'); // Cambiado a plural
-const upArrows = getElementClass('.up-arrow');     // Cambiado a plural
+    const accordionBtns = getElementClass('.accordion-btn');
+    const accordionContents = getElementClass('.accordion-content');
+    const downArrows = getElementClass('.down-arrow');
+    const upArrows = getElementClass('.up-arrow');    
 
-accordionBtns.forEach((btn, index) => {
-    btn.addEventListener('click', () => {
-        // Alternar la clase 'open' en el contenido correspondiente
-        accordionContents[index].classList.toggle('open');
+    accordionBtns.forEach((btn, index) => {
+        btn.addEventListener('click', () => {
+            accordionContents[index].classList.toggle('open');
 
-        // Alternar visibilidad de las flechas correspondientes
-        downArrows[index].classList.toggle('none');
-        upArrows[index].classList.toggle('show');
+            downArrows[index].classList.toggle('none');
+            upArrows[index].classList.toggle('show');
+        });
     });
-});
+
+
+    const getElement = (classs) => document.querySelector(classs)
+
+    const username = getElement('.username')
+    const modalDash = getElement('.modal-dash')
+
+    username.addEventListener('click', () => {
+        modalDash.classList.toggle('open')
+    })
+})
--- roles/dashboard-store.php
+++ roles/dashboard-store.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard Store - Vendex</title>
     <link rel="stylesheet" href="../css/dashboard-store.css">
-    <link rel="shortcut icon" href="../svg/icon-vendex" type="image/x-icon">
+    <link rel="shortcut icon" href="../svg/icon-vendex.svg" type="image/x-icon">
 </head>
 <body>
 
@@ -82,7 +82,44 @@
         </section>
 
         <section class="dashboard">
+            <nav class="nav-dash">
+                <h2 class="tlt-dash">Dashboard</h2>
+                <div class="noti-username">
+                    <a href="#">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path fill="#eee" d="M4 19v-2h2v-7q0-2.075 1.25-3.687T10.5 4.2V2h3v2.2q2 .5 3.25 2.113T18 10v7h2v2zm8 3q-.825 0-1.412-.587T10 20h4q0 .825-.587 1.413T12 22"/></svg>
+                        <span>9</span>
+                    </a>
+                    
+                    <div class="user-modal">
+                        <div class="username">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24"><path fill="#2ab2ed" d="M12 2c5.52 0 10 4.48 10 10s-4.48 10-10 10S2 17.52 2 12S6.48 2 12 2M6.023 15.416C7.491 17.606 9.695 19 12.16 19s4.669-1.393 6.136-3.584A8.97 8.97 0 0 0 12.16 13a8.97 8.97 0 0 0-6.137 2.416M12 11a3 3 0 1 0 0-6a3 3 0 0 0 0 6"/></svg>
+                            <div class="name-down">
+                                <p class="name">Ferney Bar.</p>
+                                <img src="../svg/down-arrow.svg" alt="">
+                            </div>
+                        </div>
 
+                        <div class="modal-dash">
+                            <div class="border-modal">
+                                <div class="name-logout">
+                                    <div class="myself">
+                                        <p class="name">Ferney Bar.</p>
+                                        <p class="rol">Store</p>
+                                    </div>
+
+                                    <div class="options-modal">
+                                        <a href="#">Salir</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+
+            <section class="control-dash">
+
+            </section>
         </section>
     </main>
 
