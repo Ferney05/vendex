82051d83830b6c53ab5aaa222d39b71e88c7f166
Ferney Barbosa <barbosaferney05@gmail.com>
2024-10-29 14:49:08 -0500

    update - vendex 2.5

--- roles/restaurant/inventory/functions/add-ingredients-dish.php
+++ roles/restaurant/inventory/functions/add-ingredients-dish.php
@@ -3,12 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Agregar recetas al inventario - Vendex</title>
-    <link rel="stylesheet" href="../../../css/restaurant/add-ingredients.css">
-    <link rel="shortcut icon" href="../../../svg/icon-vendex.svg" type="image/x-icon">
+    <title>Agregar insumos al inventario - Vendex</title>
+    <link rel="stylesheet" href="../../../../css/restaurant/add-ingredients.css">
+    <link rel="stylesheet" href="../../../../css/base-autocomplete.css">
+    <link rel="shortcut icon" href="../../../../svg/icon-vendex.svg" type="image/x-icon">
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
+    <link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
 
     <?php
-        include("../../../conexion.php");
+        include("../../../../conexion.php");
 
         session_start();
 
@@ -18,13 +23,17 @@
             header("Location: ../index.php");
             exit(); 
         }
+
+        if(isset($_GET['id'])){
+            $id_dish = $_GET['id'];
+        }
     ?>
 </head>
 <body>
 
     <main class="main">
         <nav class="nav-dash">
-            <a href="../../dashboard-restaurant.php" class="go-dash">
+            <a href="../../../dashboard-restaurant.php" class="go-dash">
                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path fill="#eee" d="m4 10l-.707.707L2.586 10l.707-.707zm17 8a1 1 0 1 1-2 0zM8.293 15.707l-5-5l1.414-1.414l5 5zm-5-6.414l5-5l1.414 1.414l-5 5zM4 9h10v2H4zm17 7v2h-2v-2zm-7-7a7 7 0 0 1 7 7h-2a5 5 0 0 0-5-5z"/></svg>
                 <p>Dashboard</p>
             </a>
@@ -40,7 +49,7 @@
                             
                             echo "<p class='name'>" . $rowData['name'] . ' ' . substr($rowData['lastname'], 0, 3) . ".</p>";
                         ?>
-                        <img src="../../../svg/down-arrow.svg" alt="">
+                        <img src="../../../../svg/down-arrow.svg" alt="">
                     </div>
                 </div>
 
@@ -59,7 +68,7 @@
                             </div>
 
                             <div class="options-modal">
-                                <a href="../../../app/users/logout.php">
+                                <a href="../../../../app/users/logout.php">
                                     <p>Salir</p>
                                     <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24"><path fill="#eee" d="M16 18H6V8h3v4.77L15.98 6L18 8.03L11.15 15H16z"/></svg>
                                 </a>
@@ -73,10 +82,10 @@
         <section class="add-products-form" id="hidden-modal">
             <div class="add-form">
                 <div class="tlt-button">
-                    <h2 class="tlt-function">Agregar recetas</h2>
+                    <h2 class="tlt-function">Agregar ingredientes al plato</h2>
                     
                     <div class="create-update">
-                        <a href="admin-inventory.php" class="button-function">
+                        <a href="../admin-inventory.php" class="button-function">
                             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path fill="#eee" d="M12 1L3 5v6c0 5.55 3.84 10.74 9 12c5.16-1.26 9-6.45 9-12V5Zm0 3.9a3 3 0 1 1-3 3a3 3 0 0 1 3-3m0 7.9c2 0 6 1.09 6 3.08a7.2 7.2 0 0 1-12 0c0-1.99 4-3.08 6-3.08"/></svg>
                             <p>Administrar inventario</p>
                         </a>
@@ -84,40 +93,51 @@
                 </div>
 
                 <div class="content-form">
-                    <form action="functions/adding-recipes.php" method="POST" class="form">
+                    <form action="adding-ingredients-dish.php?id=<?php echo $id_dish ?>" method="POST" class="form">
                         <div class="alls">
                             <div class="content-labels-inputs">
                                 <div class="label-input">
-                                    <label for="name-dish">Nombre del plato</label>
-                                    <input type="text" name="name-dish" class="input-form" placeholder="Nombre del plato" required>
+                                    <label for="name-ingredient">Nombre del ingrediente</label>
+                                    <input type="text" name="name-ingredient" id="name-ingredient" class="input-form" placeholder="Nombre del producto" required>
                                 </div>
                             </div>
 
                             <div class="content-labels-inputs">
                                 <div class="label-input">
-                                    <label for="sale-price">Precio de venta</label>
-                                    <input type="number" name="sale-price" class="input-form" placeholder="Precio de venta" required>
+                                    <label for="taken-stock">Cantidad</label>
+                                    <input type="number" name="taken-stock" class="input-form" placeholder="Cantidad que lleva" required>
                                 </div>
                             </div>
 
-                            <div class="content-labels-inputs">
-                                <div class="label-input">
-                                    <label for="prepared-time">Tiempo de preparación</label>
-                                    <input type="text" name="prepared-time" class="input-form" placeholder="Tiempo de preparación" required>
-                                </div>
+                            <div class="button-submit">
+                                <input type="submit" name="button-add-ingredient-dish" class="btn-form" value="Agregar">
                             </div>
-                        </div>
-
-                        <div class="button-submit">
-                            <input type="submit" name="button-add-recipe" class="btn-form" value="Agregar">
                         </div>
                     </form>
                 </div>
             </div>
+
+            <script>
+                $(document).ready(function(){
+                    // Inicia el autocompletado usando jQuery UI
+                    $('#name-ingredient').autocomplete({
+                        source: function(request, response) {
+                            $.ajax({
+                                url: "autocomplete.php", // Archivo PHP que consulta los productos
+                                type: "POST",
+                                data: { query: request.term }, // Envía lo que el usuario escribe
+                                success: function(data) {
+                                    response($.parseJSON(data)); // Convierte los datos en formato JSON
+                                }
+                            });
+                        },
+                        minLength: 3 // Comienza a buscar desde el segundo carácter
+                    });
+                });
+            </script>
         </section>
     </main>
 
-    <script src="../../../js/base-nav-dash.js"></script>
-
+    <script src="../../../../js/base-nav-dash.js"></script>
 </body>
 </html>
--- roles/restaurant/inventory/functions/adding-ingredients-dish.php
+++ roles/restaurant/inventory/functions/adding-ingredients-dish.php
@@ -3,9 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Receta agregada al inventario - Vendex</title>
+    <title>Agregar ingredientes al plato - Vendex</title>
     <link rel="stylesheet" href="../../../../css/success.css">
     <link rel="shortcut icon" href="../../../../svg/icon-vendex.svg" type="image/x-icon">
+
+    <?php
+        include("../../../../conexion.php");
+
+        if(isset($_GET['id'])){
+            $id_dish = $_GET['id'];
+        }
+    ?>
 </head>
 <body>
 
@@ -13,25 +21,25 @@
 
         include("../../../../conexion.php");
 
-        if(isset($_POST['button-add-recipe'])) {
-            $name_dish = $_POST['name-dish'];
-            $sale_price = $_POST['sale-price'];
-            $time = $_POST['prepared-time'];
+        if(isset($_POST['button-add-ingredient-dish'])) {
+            $id_dish = $_GET['id'];
+            $name_ingredient = $_POST['name-ingredient'];
+            $taken_stock = $_POST['taken-stock'];
 
-            $insertData = "INSERT INTO recipes VALUES(null, '$name_dish', '$sale_price', '$time')";
+            $insertData = "INSERT INTO ingredients_of_dish VALUES(null, $id_dish, '$name_ingredient', '$taken_stock')";
             $execute = mysqli_query($conexion, $insertData);
 
             if($execute){
-                header("Refresh: 3; url=../add-recipes.php");
+                header("Refresh: 3; url=add-ingredients-dish.php");
 
                 echo "<div class='success'>
                         <div class='back'>
                             <div class='tlt-icon'>
-                                <h2>Receta agregada</h2>
+                                <h2>Ingrediente agregado</h2>
                                 <svg xmlns='http://www.w3.org/2000/svg' width='40' height='40' viewBox='0 0 48 48'><path fill='#6bc04e' fill-rule='evenodd' stroke='#6bc04e' stroke-linecap='round' stroke-linejoin='round' stroke-width='4' d='m4 24l5-5l10 10L39 9l5 5l-25 25z' clip-rule='evenodd'/></svg>
                             </div>
 
-                            <p class='info-text'>Receta añadida al inventario.</p>
+                            <p class='info-text'>Se agregó correctamente el ingrediente al plato.</p>
                         </div>
                       </div>";
                     
